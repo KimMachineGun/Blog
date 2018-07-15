9139e942feb3abb2491d43631e4150e8816279a4
KimMachineGun <geon0250@gmail.com>
2018-07-15 23:26:15 +0900

    [2018-07-15 23:26:14.91] Auto Commit

--- AutoCommit.bat
+++ AutoCommit.bat
@@ -1,14 +1,16 @@
 @echo off
 hugo
 
+if %time:~0,1% == " " set time=0%time%
+
 git add .
-git commit -m "[%date%%time%] Auto Commit"
+git commit -m "[%date% %time%] Auto Commit"
 git push origin master
 
 cd public
 
 git add .
-git commit -m "[%date%%time%] Auto Commit"
+git commit -m "[%date% %time%] Auto Commit"
 git push origin master
 
 cd ..
